51b0bfe763ea8919891600cc0d8a9e01e54e51fa
Luis Guilherme <luis.vb@pti.org.br>
2025-01-16 14:45:58 -0300

    First Commit

---
+++ Assets/Hands/Scripts/HandController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandController : MonoBehaviour
+{
+    ActionBasedController controller;
+    public Hand hand;
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hand.SetGrip(controller.selectAction.action.ReadValue<float>());
+        hand.SetTrigger(controller.activateAction.action.ReadValue<float>());
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DisjuntoresVR
+ 
